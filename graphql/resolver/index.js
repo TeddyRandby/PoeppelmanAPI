f746442e580b3b2d224d2a5934456407053541f5
Teddy <tedrandby@gmail.com>
2020-11-11 09:38:49 -0500

    fix uri

--- graphql/resolver/index.js
+++ graphql/resolver/index.js
@@ -4,7 +4,6 @@ const testResolver = require('./test');
 const rootResolver = {
   ...poeppelmanResolver,
   ...testResolver
-
 };
 
 module.exports = rootResolver;
--- server.js
+++ server.js
@@ -1,43 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
-const graphqlSchema = require('./graphql/schema/index');
-const graphqlResolver = require('./graphql/resolver/index');
-const mongoose = require('mongoose');
-const mongoURI = process.env.MONGODB_URI || "mongodb://APIConnection:daniela12#@ds343895.mlab.com:43895/heroku_1wp0hgxn";
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const graphqlSchema = require("./graphql/schema/index");
+const graphqlResolver = require("./graphql/resolver/index");
+const mongoose = require("mongoose");
+const mongoURI = process.env.MONGODB_URI;
 const port = process.env.PORT || 8000;
-
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
   next();
 });
-app.use('/api', graphqlHttp({
-  
-  schema: graphqlSchema,
-  rootValue: graphqlResolver,
-  graphiql: true
 
-}));
+app.use(
+  "/api",
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
+);
 
 mongoose
-  .connect(
-    mongoURI, {
-      useNewUrlParser: true
-    }
-  ).then(() => {
-    app.listen(port, () => console.log(`Poeppelman Graphql API listening on port ${port}!`))
+  .connect(mongoURI, {
+    useNewUrlParser: true,
   })
-  .catch( err => {
+  .then(() => {
+    app.listen(port, () =>
+      console.log(`Poeppelman Graphql API listening on port ${port}!`)
+    );
+  })
+  .catch((err) => {
     console.log(err);
   });
